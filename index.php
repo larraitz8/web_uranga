33b05a01b5857810a09eb4f272ddda48120ddaf4
larraitz8 <larraitzo8@hotmail.com>
2021-04-18 14:38:52 +0200

    cambios de tabs a carpetas

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <?php include 'head.php';?>
+    <?php include 'tabs/head.php';?>
 </head>
 <body id="page-top">    
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-       <?php include 'menu.php';?>
+       <?php include 'tabs/menu.php';?>
     </nav>
     <!-- Masthead-->
     <header class="masthead">
-        <?php include 'header.php';?>
+        <?php include 'tabs/header.php';?>
     </header>
     <!-- Services-->
     <section class="page-section" id="services">
-        <?php include 'servicios.php';?>
+        <?php include 'tabs/servicios.php';?>
     </section>
 
     <section class="page-section" id="tecnicas">
-        <?php include 'tecnicas.php';?>
+        <?php include 'tabs/tecnicas.php';?>
     </section>
     <!-- Team-->
     <section class="page-section bg-light" id="about">
-        <?php include 'about.php';?>
+        <?php include 'tabs/about.php';?>
     </section>
     <section class="page-section" id="contact" style="padding-top: 30px; padding-bottom: 30px";>
-        <?php include 'contacto.php';?>
+        <?php include 'tabs/contacto.php';?>
     </section>
     <!-- Footer-->
-    <?php include 'footer.php';?>
+    <?php include 'tabs/footer.php';?>
 
     <!-- Bootstrap core JS-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
